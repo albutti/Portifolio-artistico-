2aeb707ef4bd0379f4f7a88f1f5a76b5ce5a2ce2
Caique Albutti <caique.albutti@hotmail.com>
2025-04-26 07:13:22 -0300

    artistico att

---
+++ js/script.js
@@ -0,0 +1,132 @@
+$('.owl-carousel').owlCarousel({
+    loop:false,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+/** Ator */
+
+function abrir_popup_tempestade(){
+    document.getElementById('popup_tempestade').style.display = 'block';
+    document.body.style.overflow = "hidden";
+}
+
+function fechar_popup_tempestade(){
+    document.getElementById('popup_tempestade').style.display = 'none';
+    document.body.style.overflow = "scroll";
+}
+
+function abrir_popup_assis(){
+    document.getElementById('popup_assis').style.display = 'block';
+    document.body.style.overflow = "hidden";
+}
+
+function fechar_popup_assis(){
+    document.getElementById('popup_assis').style.display = 'none';
+    document.body.style.overflow = "scroll";
+}
+
+function abrir_popup_sitio(){
+    document.getElementById('popup_sitio').style.display = 'block';
+    document.body.style.overflow = "hidden";
+}
+
+function fechar_popup_sitio(){
+    document.getElementById('popup_sitio').style.display = 'none';
+    document.body.style.overflow = "scroll";
+}
+
+function abrir_popup_visita(){
+    document.getElementById('popup_visita').style.display = 'block';
+    document.body.style.overflow = "hidden";
+}
+
+function fechar_popup_visita(){
+    document.getElementById('popup_visita').style.display = 'none';
+    document.body.style.overflow = "scroll";
+}
+
+function abrir_popup_leais(){
+    document.getElementById('popup_leais').style.display = 'block';
+    document.body.style.overflow = "hidden";
+}
+
+function fechar_popup_leais(){
+    document.getElementById('popup_leais').style.display = 'none';
+    document.body.style.overflow = "scroll";
+}
+
+function abrir_popup_benvinda(){
+    document.getElementById('popup_benvinda').style.display = 'block';
+    document.body.style.overflow = "hidden";
+}
+
+function fechar_popup_benvinda(){
+    document.getElementById('popup_benvinda').style.display = 'none';
+    document.body.style.overflow = "scroll";
+}
+
+/** Iluminação */
+
+function abrir_popup_lis(){
+    document.getElementById('popup_lis').style.display = 'block';
+    document.body.style.overflow = "hidden";
+}
+
+function fechar_popup_lis(){
+    document.getElementById('popup_lis').style.display = 'none';
+    document.body.style.overflow = "scroll";
+}
+
+function abrir_popup_abracadabra(){
+    document.getElementById('popup_abracadabra').style.display = 'block';
+    document.body.style.overflow = "hidden";
+}
+
+function fechar_popup_abracadabra(){
+    document.getElementById('popup_abracadabra').style.display = 'none';
+    document.body.style.overflow = "scroll";
+}
+
+function abrir_popup_magico(){
+    document.getElementById('popup_magico').style.display = 'block';
+    document.body.style.overflow = "hidden";
+}
+
+function fechar_popup_magico(){
+    document.getElementById('popup_magico').style.display = 'none';
+    document.body.style.overflow = "scroll";
+}
+
+/** duble */
+
+function abrir_popup_dna2(){
+    document.getElementById('popup_dna2').style.display = 'block';
+    document.body.style.overflow = "hidden";
+}
+
+function fechar_popup_dna2(){
+    document.getElementById('popup_dna2').style.display = 'none';
+    document.body.style.overflow = "scroll";
+}
+
+function abrir_popup_rio_sangue(){
+    document.getElementById('popup_rio_sangue').style.display = 'block';
+    document.body.style.overflow = "hidden";
+}
+
+function fechar_popup_rio_sangue(){
+    document.getElementById('popup_rio_sangue').style.display = 'none';
+    document.body.style.overflow = "scroll";
+}
